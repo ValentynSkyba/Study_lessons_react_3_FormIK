14c3b627d5b2729532cb33ee01585cb8b0faa326
ValentynSkyba <valentynskyba@gmail.com>
2024-04-06 00:49:38 +0100

    add form

--- src/components/BookStore/Components/AddForm/AddForm.jsx
+++ src/components/BookStore/Components/AddForm/AddForm.jsx
@@ -1,5 +1,65 @@
+import { Field, Form, Formik } from "formik";
+import { nanoid } from "nanoid";
+
 const AddForm = () => {
-  return <div></div>;
+  const initialValues = {
+    name: "",
+    author: "",
+    description: "",
+  };
+
+  const handleSubmit = (data, options) => {
+    console.log({ ...data, id: nanoid() });
+    options.resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form className="form addForm">
+        <label className="label">
+          Book name:
+          <Field
+            type="text"
+            name="name"
+            className="input"
+            placeholder="Enter name"
+          />
+        </label>
+        <label className="label">
+          Book name:
+          <Field
+            type="text"
+            name="name"
+            className="input"
+            placeholder="Enter name"
+          />
+        </label>
+        <label className="label">
+          Book author:
+          <Field
+            type="text"
+            name="author"
+            className="input"
+            placeholder="Enter name"
+          />
+        </label>
+        <label className="label">
+          Book author:
+          <Field
+            as="textarea"
+            rows="3"
+            cols="20"
+            name="description"
+            className="input"
+            placeholder="Enter name"
+          />
+        </label>
+        <button type="submit" className="btn border">
+          Add book
+        </button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default AddForm;
